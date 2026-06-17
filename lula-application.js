f73f259b4ccfe8db625a999418bc7a96f04b9185
Paul Haury <5140929+phaury@users.noreply.github.com>
2026-06-17 13:46:36 -0700

    Update lula-application.js
    v12 rollback

--- lula-application.js
+++ lula-application.js
@@ -7,7 +7,7 @@
   }
 
   const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbwUP104700QQI9HaqjcEGnaHD8qkN5vCSoRdhyoUio4ytCjHqJrOl-dDpwVryfNxUqq/exec";
+    "https://script.google.com/macros/s/AKfycbyVcPYXLNunkMY7m-1WTAZKbD1syejUpsZratVN3YoIkUhiukea2457iwHSEUwnZkUvEA/exec";
 
   const LULAOLOGY_URL =
     "https://drive.google.com/file/d/1vh_0WWNJ-XNRZycdiMT-T1LoEVucWzsr/view?usp=sharing";
