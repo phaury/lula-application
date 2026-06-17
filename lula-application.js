5161e954d43ba9ddfcc406e98d41c13fd1b58296
Paul Haury <5140929+phaury@users.noreply.github.com>
2026-06-16 21:56:56 -0700

    Update lula-application.js
    V12 naming convention

--- lula-application.js
+++ lula-application.js
@@ -7,7 +7,7 @@
   }
 
   const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbwkCHBbcU_sMw97OysViFXR7UdLDpcXflSRf9MheEb7YghuFnFoVcpohUUmjQ3vKvrC/exec";
+    "https://script.google.com/macros/s/AKfycbyVcPYXLNunkMY7m-1WTAZKbD1syejUpsZratVN3YoIkUhiukea2457iwHSEUwnZkUvEA/exec";
 
   const LULAOLOGY_URL =
     "https://drive.google.com/file/d/1vh_0WWNJ-XNRZycdiMT-T1LoEVucWzsr/view?usp=sharing";
