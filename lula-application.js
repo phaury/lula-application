cbb11cf2bccdb8989305b39f5d218014bc167e47
Paul Haury <5140929+phaury@users.noreply.github.com>
2026-06-17 15:43:37 -0700

    Update lula-application.js
    Clean code re-uploaded now version 20

--- lula-application.js
+++ lula-application.js
@@ -7,7 +7,7 @@
   }
 
   const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycby8i9CcmGVgTJlg7QKZEGmIAR7CI5XjI2B2aOgZINOD_pyLNeoUjGA-Ixopx84zd1ysUA/exec";
+    "https://script.google.com/macros/s/AKfycbygRrKO-ewp8PQ4bK15sqotJtMHDjPZQPx0c099B5TCJOaGY2fgyBnz6AAWBcfIWpsq0g/exec";
 
   const LULAOLOGY_URL =
     "https://drive.google.com/file/d/1vh_0WWNJ-XNRZycdiMT-T1LoEVucWzsr/view?usp=sharing";
