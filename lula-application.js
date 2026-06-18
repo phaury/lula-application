4366ad9bd27e899cd17616db5aec97fb5887f8c2
Paul Haury <5140929+phaury@users.noreply.github.com>
2026-06-17 21:03:16 -0700

    Update lula-application.js
    File uproot rename fix v27

--- lula-application.js
+++ lula-application.js
@@ -7,7 +7,7 @@
   }
 
   const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbyud6alnqCWR6GOtr-6KlEfOSj6WqbyBw7cf7g6_qyBz_jrTWnz7YQ8D24pXtOf18tbgg/exec";
+    "https://script.google.com/macros/s/AKfycbwYpDyk3RcpR9mUYZToxBGbwVxDKsz6Ok2aNZ3nRCKi9CPUMhWpXb-518vVV-RokvAJng/exec";
 
   const LULAOLOGY_URL =
     "https://drive.google.com/file/d/1vh_0WWNJ-XNRZycdiMT-T1LoEVucWzsr/view?usp=sharing";
