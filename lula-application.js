76c2eea9cc477fa8e143eab9760c7e07ad238a2f
Paul Haury <5140929+phaury@users.noreply.github.com>
2026-06-18 15:40:08 -0700

    Update lula-application.js
    Insert record into first row v29

--- lula-application.js
+++ lula-application.js
@@ -7,7 +7,7 @@
   }
 
   const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbxn-jPy60FDapzXqF0KPLJn1Vfs9DtmHYMNHGUnKz_UownzOWSKbMmiY944gHgncH8dbw/exec";
+    "https://script.google.com/macros/s/AKfycby6zKo2oAXYQRjLw3iPdHokYXAFnb_EM2i-iYor_PB9wZSEjt2NumjNb7rJ7r4ffAia8Q/exec";
 
   const LULAOLOGY_URL =
     "https://drive.google.com/file/d/1vh_0WWNJ-XNRZycdiMT-T1LoEVucWzsr/view?usp=sharing";
