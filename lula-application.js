299db7beb2eb13522b88a00d41ed185d20d417c1
Paul Haury <5140929+phaury@users.noreply.github.com>
2026-06-25 21:25:33 -0700

    Update lula-application.js
    New cool stuff

--- lula-application.js
+++ lula-application.js
@@ -7,7 +7,7 @@
   }
 
   const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycby5odudGDrA0Bl7oBe8qUW4oU7aq_vDIE0vtYes5_j-L42jzn4jHbZGK7BZREryEBfk-g/exec";
+    "https://script.google.com/macros/s/AKfycbx9pyoZML9F_UK97awEueWorDXDqUtQjwlsCx6JobHoh4Y0XVlGV-5sdrKGjHa5YcZ7_A/exec";
 
   const LULAOLOGY_URL =
     "https://drive.google.com/file/d/1vh_0WWNJ-XNRZycdiMT-T1LoEVucWzsr/view?usp=sharing";
